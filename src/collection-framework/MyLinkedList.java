0c9e6bd7428150ac5ea2b7fa1e6639bc8f035031
Saurabh Shah <33320357+saurabh2001@users.noreply.github.com>
2021-09-25 23:50:11 +0530

    Adding own implementation of linked list

---
+++ src/collection-framework/MyLinkedList.java
@@ -0,0 +1,67 @@
+package collection-framework;
+
+import java.util.Objects;
+
+public class MyLinkedList {
+    int size;
+    Node head;
+
+    public MyLinkedList(){
+         this.size = 0;
+    }
+
+    public void addToEnd(String item){
+        Node newNode = new Node(item);
+
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        Node lastNode = head;
+        //traverse till last node
+        while(lastNode.next != null){
+            lastNode = lastNode.next;
+        }
+        //attach newly created node to end
+        lastNode.next = newNode;
+    }
+
+    public void addToStart(String item){
+        Node newNode = new Node(item);
+
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        // attach new node before the head
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void print(){
+        Node n = head;
+        while(n != null){
+            System.out.println(n.item + " ");
+            n = n.next;
+        }
+    }
+
+    private class Node{
+        String item;
+        Node next;
+
+        Node(String item){
+            this.item = item;
+            this.next = null;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "item='" + item + '\'' +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+}
