828c104f5ebb9d494718e21ab81f7fd17eba9592
saurabh <shahsaurabh@johndeere.com>
2021-10-04 18:54:09 +0530

    strategy design pattern implementation

---
+++ src/designpatterns/strategyDP/PaymentStrategy.java
@@ -0,0 +1,5 @@
+package designpatterns.strategyDP;
+
+public interface PaymentStrategy {
+    void pay(int amount);
+}
---
+++ src/designpatterns/strategyDP/PaypalStrategy.java
@@ -0,0 +1,17 @@
+package designpatterns.strategyDP;
+
+public class PaypalStrategy implements PaymentStrategy{
+    private String email;
+    private String password;
+
+    public PaypalStrategy(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+
+    @Override
+    public void pay(int amount) {
+        System.out.println(amount + " paid using Paypal.");
+    }
+}
