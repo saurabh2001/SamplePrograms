9387a44d2d24c3ec080a7989029f7959317da48d
saurabh <shahsaurabh@johndeere.com>
2021-10-07 11:29:54 +0530

    sync singleton

---
+++ src/designpatterns/singleton/sync/LazySyncSingleton.java
@@ -0,0 +1,22 @@
+package designpatterns.singleton.sync;
+
+public class LazySyncSingleton {
+    // lazy initialization
+    private static LazySyncSingleton singleton ;
+
+    //private constructor to avoid client applications to use constructor
+    private LazySyncSingleton(){
+
+    }
+
+    // putting synchronized, Only one thread can execute this at a time,
+    // but it is costly because to get same instance each method has to wait, synchronized is only required on 1st call
+    // so we use double checking locking
+    // Ref: https://www.geeksforgeeks.org/singleton-design-pattern/
+    public static synchronized LazySyncSingleton getEagerInstance(){
+        if(singleton == null){
+            singleton = new LazySyncSingleton();
+        }
+        return singleton;
+    }
+}
---
+++ src/designpatterns/singleton/sync/LazySyncSingletonClient.java
@@ -0,0 +1,10 @@
+package designpatterns.singleton.sync;
+
+public class LazySyncSingletonClient {
+    public static void main(String[] args) {
+
+        System.out.println("Following is the call to get the singleton instance lazy+sync way");
+        LazySyncSingleton singleton = LazySyncSingleton.getEagerInstance();
+
+    }
+}
