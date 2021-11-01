dd2272846d79070cb8f7e2b7597f35e3f3af8321
saurabh <shahsaurabh@johndeere.com>
2021-11-01 16:12:00 +0530

    Identifying if the String is balanced

--- src/BalancedString.java
+++ src/BalancedString.java
@@ -3,23 +3,44 @@ import java.util.Stack;
 
 public class BalancedString {
     public static void main(String[] args) {
-        HashMap<String,String> map = new HashMap();
+        HashMap<String,String> map = new HashMap<>();
         map.put("{","}");
         map.put("(",")");
         map.put("[","]");
-        Stack st = new Stack();
+        Stack<String> stack = new Stack<>();
 
         String str = "{([asdasd]dasda)asdasd}";
 
         int len = str.length();
-        boolean isValid = false;
+        boolean isBalanced = true;
 
-        for (int i = 0; i < len-1; i++) {
+        for (int i = 0; i < len; i++) {
             String c =  Character.toString(str.charAt(i));
-
-
-
-            System.out.println(c);
+            System.out.print("------Processing: "+ c);
+            if(map.containsKey(c)) {
+                System.out.println("  Pushing into stack");
+                stack.push(c);
+            }
+            else if(map.containsValue(c)) {
+                System.out.println("  Popping from stack");
+                if(stack.isEmpty()) {
+                    System.out.println("  Stack is empty");
+                    isBalanced = false;
+                    break;
+                }
+                String value = map.get(stack.pop());
+                if(!value.equals(c)) {
+                    isBalanced = false;
+                    break;
+                }
+            }
+            else {
+                System.out.println("  Ignoring the characters");
+            }
         }
+        if(isBalanced)
+            System.out.println("String is balanced");
+        else
+            System.out.println("String is NOT balanced");
     }
 }
